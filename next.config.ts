999b2d7af7f3d2297f6d5c0c96b6980a8238af91
Ahmet Can Ceylan <116285492+ahmetcanc@users.noreply.github.com>
2026-02-26 15:59:38 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  output: 'export',      // Statik dosyaların (HTML/CSS/JS) oluşturulmasını sağlar
-  images: {
-    unoptimized: true,   // GitHub Pages'da resimlerin hata vermemesi için gereklidir
-  }, 
+  reactStrictMode: true 
 };
  
 export default nextConfig;
