42e1a9ed6f9d6d22fd2fc44553636ece37335da0
Ahmet Can Ceylan <116285492+ahmetcanc@users.noreply.github.com>
2026-02-27 13:59:18 +0300

    Update next.config.ts for static export and images
    Enable static file export and configure image optimization.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+    output: 'export',      // Statik dosyaların (HTML/CSS/JS) oluşturulmasını sağlar
+  images: {
+    unoptimized: true,   // GitHub Pages'da resimlerin hata vermemesi için gereklidir
+  },
 };
  
 export default nextConfig;
